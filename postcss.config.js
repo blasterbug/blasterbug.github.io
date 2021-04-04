c097fd29899b4f9a86840973361a62618b76ba2b
Benjamin <blasterbug@users.noreply.github.com>
2021-04-04 15:00:37 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,27 @@
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('precss'),
-    require('autoprefixer'),
-  ]
-}
+    'autoprefixer',
+    'tailwindcss',
+    // require('cssnano')({
+    //   preset: [
+    //     'advanced',
+    //     {
+    //       autoprefixer: false,
+    //       discardComments: {
+    //         removeAll: true,
+    //       },
+    //     },
+    //   ],
+    // }),
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        stage: 1,
+        features: {
+          'focus-within-pseudo-class': false,
+        },
+      },
+    ],
+  ],
+};
