2a30e1b6ccd674d34f7bf2753b728e87850994a1
Benjamin <blasterbug@users.noreply.github.com>
2021-04-05 16:08:42 +0200

    Create Background.jsx

---
+++ src/Home/Background.jsx
@@ -0,0 +1,273 @@
+import React from 'react';
+
+function Background() {
+  return (
+    <div className="relative py-16 lg:px-8 bg-gray-50 overflow-hilien">
+      <div
+        id="background"
+        className="relative h-full text-lg max-w-prose mx-auto"
+      >
+        <div className="mt-6 px-4 prose prose-blue prose-lg text-gray-500 mx-auto">
+          <h2>Background</h2>
+          <h3 id="cur-2018">2018</h3>
+          <ul>
+            <h4>May</h4>
+            <li>
+              Nesta speeched as one of the three most promising start-ups
+              at&nbsp;
+              <a
+                href="https://umea-tech-arena-may2018.confetti.events/"
+                title="Umeå Tech Arena - Artificial intelligence - the new normal"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Umeå Tech Arena
+              </a>
+              .
+            </li>
+            <li>
+              <a
+                href="https://cloudplatformonline.com/2018-OnBoard-stockholm-q2.html"
+                title="Cloud OnBoard - Stockholm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Google Cloud OnBoard
+              </a>
+              , Stockholm.
+            </li>
+          </ul>
+          <h3 id="cur-2017">2017</h3>
+          <ul>
+            <h4>October</h4>
+            <li>
+              Nesta is selected for&nbsp;
+              <a
+                title="Di Startup Tours 2017"
+                href="https://digital.di.se/artikel/de-tavlar-i-startup-tours-division-nord-i-umea"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Di Startup Tours
+              </a>
+              .
+            </li>
+            <li>Launching the close beta of Nesta.</li>
+            <h4>June</h4>
+            <li>
+              Joining &nbsp;
+              <a
+                href="http://www.seashepherdscandinavia.org/"
+                title="Sea Shepherd Scandinavia"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Sea Shepherd Scandinavia
+              </a>
+              &nbsp; as a volunteer.
+            </li>
+            <h4>April</h4>
+            <li>Learning Swedish at SFI school.</li>
+            <h4>March</h4>
+            <li>
+              Joined the Nesta team as a cofounder and a fullstack developer.
+            </li>
+          </ul>
+          <h3 id="cur-2016">2016</h3>
+          <ul>
+            <h4>October</h4>
+            <li>
+              Start working as a fullstack developer at Thingmap (now&nbsp;
+              <a
+                href="http://qui.ai/"
+                title="Qui"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Qui
+              </a>
+              ). I extended the RESTful API (backend), I improved and
+              implemented new features to the frontend applications (web and
+              mobile). I dealt with Node.js, AngularJS, REACT and MongoDB. I
+              also wrote a lot of tests and fixed bugs.
+            </li>
+            <h4>May</h4>
+            <li>Umeå Tech Arena.</li>
+            <li>
+              Django workshop at&nbsp;
+              <a href="http://www.codemill.se/">Codemill</a>.
+            </li>
+          </ul>
+          <h3 id="cur-2015">2015</h3>
+          <ul>
+            <h4>September</h4>
+            <li>
+              Master's degree, first year in Computer Science at Umeå University
+              as an exchange student. Where I studied Aritficial Intelligence,
+              Databases and Distributed Systems.
+            </li>
+            <h4>August</h4>
+            <li>Moved to Umeå, Sweden.</li>
+            <li>
+              Graduated from the University of Nantes in Computer Science.
+            </li>
+            <h4>April - July</h4>
+            <li>
+              Third internship within the Real Time Systems team of the&nbsp;
+              <a
+                href="http://www.irccyn.ec-nantes.fr/en/irccyn/presentation"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Institut de Recherche en Communication et Cybernétique de Nantes"
+              >
+                IRCCyN
+              </a>
+              &nbsp;leaded by Richard Urunuela, still about the ANR ImpRo
+              demonstrator. This time, my task was to build a new robot with
+              metal parts.
+            </li>
+          </ul>
+
+          <div className="py-6 w-full">
+            <iframe
+              className="m-auto"
+              width="480"
+              height="360"
+              src="https://www.youtube.com/embed/91nHZ1eDwN4?rel=0"
+              frameBorder="0"
+            ></iframe>
+          </div>
+
+          <h3 id="cur-2014">2014</h3>
+          <ul>
+            <h4>September</h4>
+            <li>
+              Volunteering for the thirteenth edition of&nbsp;
+              <a
+                href="http://www.scopitone.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Scoptione"
+              >
+                Scopitone
+              </a>
+              , a festival about Digital art.
+            </li>
+
+            <h4>June - July (12 weeks)</h4>
+            <li>
+              Second internship at&nbsp;
+              <a
+                href="http://www.irccyn.ec-nantes.fr/en/irccyn/presentation"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Institut de Recherche en Communication et Cybernétique de Nantes"
+              >
+                IRCCyN
+              </a>
+              &nbsp; with the Real Time System team, leaded by Richard Urunuela.
+              The purpose was adding a camera to the&nbsp;
+              <a
+                href="https://en.wikipedia.org/wiki/Lego_Mindstorms_NXT"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Lego Mindstorms NXT on Wikipedia"
+              >
+                Lego Mindstorms NXT
+              </a>
+              . The NXT is used to control the robots of the ANR ImpRo
+              demonstrator.
+              <br /> The camera is actually an&nbsp;
+              <a
+                href="http://arduino.cc/en/Mayn/ArduinoBoardUno"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Arduino Uno"
+              >
+                Arduino Uno
+              </a>
+              &nbsp; board connected to a&nbsp;
+              <a href="http://www.cmucam.org/projects/cmucam4/">CMUcam4</a> and
+              five proximity sensors. The Arduino board manages the CMU cam and
+              the sensors and send the data to the NXT via I2C.
+            </li>
+          </ul>
+
+          <h3 id="cur-2013">2013</h3>
+          <ul>
+            <h4>September</h4>
+            <li>
+              Start my bachelor degree in Computer Science at the University of
+              Nantes, France.
+            </li>
+            <li>Start part-time working as a cashier at a mini-market.</li>
+            <li>
+              Volunteering for the twelfth edition of&nbsp;
+              <a
+                href="http://www.scopitone.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Scoptione"
+              >
+                Scopitone
+              </a>
+              , a festival about Digital art.
+            </li>
+            <h4>July</h4>
+            <li>
+              Got my two-years degree in software engineering (DUT : Diplôme
+              Universitaire de Technologie Informatique) from The University of
+              Nantes, France.
+            </li>
+
+            <h4>May - June (10 weeks)</h4>
+            <li>
+              First internship at&nbsp;
+              <a
+                href="http://www.irccyn.ec-nantes.fr/en/irccyn/presentation"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Institut de Recherche en Communication et Cybernétique de Nantes"
+              >
+                IRCCyN
+              </a>
+              , within the Real Time Systems team. I was leaded by Jean-Luc
+              Béchennec, Sébastien Faucou and Richard Urunuela. During this
+              internship, I designed a first prototype for the demonstrator of
+              the ANR ImpRo project. The demonstrator is a table where two
+              robots hit back and forth a ball. It is a kind of an air-hockey.
+              Robots are built with Lego and controlled by the&nbsp;
+              <a
+                href="https://en.wikipedia.org/wiki/Lego_Mindstorms_NXT"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Lego Mindstorms NXT on Wikipedia"
+              >
+                Lego Mindstorms NXT
+              </a>
+              .&nbsp;
+              <a
+                href="http://trampoline.rts-software.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Trampoline - Open Source RTOS Project"
+              >
+                Trampoline RTOS
+              </a>
+              , the operating system developed by the team, allows the robot to
+              be autonomous with Real Time features. It runs on the NXT.
+            </li>
+          </ul>
+
+          <h3 id="cur-2011">2011</h3>
+          <ul>
+            <h4>June</h4>
+            <li>Benjamin registered on GitHub.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Background;
