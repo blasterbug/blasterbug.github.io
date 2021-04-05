4fb63a42ad5f470373aa037a66210e42bca7ea6f
Benjamin <blasterbug@users.noreply.github.com>
2021-04-05 16:08:47 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'tailwindcss/tailwind.css';
-
-import Home from './Home';
-import Footer from './Footer';
 
 import './app.css';
 
+const FourOFour = lazy(() => import('./FourOFour'));
+const Home = lazy(() => import('./Home'));
+
 function App() {
   return (
-    <>
-      <Home />
-      <Footer />
-    </>
+    <Router>
+      <Suspense fallback="">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/404">
+            <FourOFour />
+          </Route>
+          <Route>
+            <FourOFour />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
