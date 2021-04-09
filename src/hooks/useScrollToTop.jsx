f830b0347db76bf2a8effdf02f4e0408a65329a6
Benjamin <blasterbug@users.noreply.github.com>
2021-04-09 19:43:52 +0200

    Create useScrollToTop.jsx

---
+++ src/hooks/useScrollToTop.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+function useScrollToTop() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }, [location]);
+}
+
+export default useScrollToTop;
