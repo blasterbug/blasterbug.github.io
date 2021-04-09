77cf586624f37ab4086959adf86728723b7b264d
Benjamin <blasterbug@users.noreply.github.com>
2021-04-09 19:43:47 +0200

    Adding icons

--- src/Home/Skills.jsx
+++ src/Home/Skills.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
+import {
+  BeakerIcon,
+  ChatIcon,
+  DatabaseIcon,
+  DesktopComputerIcon,
+  DeviceMobileIcon,
+  PencilIcon,
+  SparklesIcon,
+  TemplateIcon,
+  TerminalIcon,
+  ViewGridIcon,
+} from '@heroicons/react/solid';
 
 function Skills() {
   return (
@@ -6,22 +18,39 @@ function Skills() {
       <div id="skills" className="relative h-full text-lg max-w-prose mx-auto">
         <div className="mt-6 px-4 prose prose-blue prose-sm sm:prose lg:prose-lg xl:prose-xl text-gray-500 mx-auto">
           <h1 className="font-display">Skills</h1>
-          <h2 className="font-display">Database</h2>
+          <h2 className="font-display inline-flex">
+            <DatabaseIcon className="h-6 w-6 lg:h-10 lg:w-10 mr-4 text-gray-300" />
+            <span>Database</span>
+          </h2>
           <ul>
-            <li>MongoDB, SQL, PL/SQL, noSQL</li>
+            <li>
+              <b>noSQL</b> MongoDB
+            </li>
+            <li>
+              <b>SQL</b> PostgreSQL, PL/SQL
+            </li>
             <li>JSON, XML, Xpath</li>
           </ul>
-          <h2 className="font-display">Designing</h2>
+          <h2 className="font-display inline-flex">
+            <ViewGridIcon className="h-6 w-6 lg:h-10 lg:w-10 mr-4 text-gray-300" />
+            <span>Designing</span>
+          </h2>
           <ul>
             <li>UML, Design Patterns, Merise, Petri nets</li>
           </ul>
 
-          <h2 className="font-display">Programming</h2>
+          <h2 className="font-display inline-flex">
+            <TerminalIcon className="h-6 w-6 lg:h-10 lg:w-10 mr-4 text-gray-300" />
+            <span>Programming</span>
+          </h2>
           <ul>
             <li>JavaScript, Python, C, C++, Java, Bash, OCaml, ADA95</li>
             <li>Assembly language (MIPS, x86), Arduino, Processing</li>
           </ul>
-          <h2 className="font-display">Specific skills</h2>
+          <h2 className="font-display inline-flex">
+            <SparklesIcon className="h-6 w-6 lg:h-10 lg:w-10 mr-4 text-gray-300" />
+            <span>Specific skills</span>
+          </h2>
           <ul>
             <li>
               Distributed Systems development (RESTful and SOAP applications)
@@ -30,7 +59,10 @@ function Skills() {
             <li>Network and UNIX systems administration</li>
             <li>Artificial Intelligence</li>
           </ul>
-          <h2 className="font-display">Tools</h2>
+          <h2 className="font-display inline-flex">
+            <BeakerIcon className="h-6 w-6 lg:h-10 lg:w-10 mr-4 text-gray-300" />
+            <span>Tools</span>
+          </h2>
           <ul>
             <li>git, npm, Docker, babel, eslint</li>
             <li>Doxygen, JUnit, Maven, Mailgun</li>
@@ -39,29 +71,44 @@ function Skills() {
               Engine)
             </li>
           </ul>
-          <h2 className="font-display">UX/UI design</h2>
+          <h2 className="font-display inline-flex">
+            <TemplateIcon className="h-6 w-6 lg:h-10 lg:w-10 mr-4 text-gray-300" />
+            <span>UX/UI design</span>
+          </h2>
           <ul>
             <li>Sketch (basic usage)</li>
             <li>Figma</li>
           </ul>
-          <h2 className="font-display">Web</h2>
+          <h2 className="font-display inline-flex">
+            <DesktopComputerIcon className="h-6 w-6 lg:h-10 lg:w-10 mr-4 text-gray-300" />
+            <span>Web</span>
+          </h2>
           <ul>
             <li>HTML, (S)CSS, JavaScript</li>
             <li>Node.js, React.js (Redux, react-router)</li>
             <li>TailwindCSS</li>
             <li>Jekyll, Django</li>
           </ul>
-          <h2 className="font-display">Mobile</h2>
+          <h2 className="font-display inline-flex">
+            <DeviceMobileIcon className="h-6 w-6 lg:h-10 lg:w-10 mr-4 text-gray-300" />
+            <span>Mobile</span>
+          </h2>
           <ul>
             <li>React-native</li>
           </ul>
-          <h2 className="font-display">Writing</h2>
+          <h2 className="font-display inline-flex">
+            <PencilIcon className="h-6 w-6 lg:h-10 lg:w-10 mr-4 text-gray-300" />
+            <span>Writing</span>
+          </h2>
           <ul>
             <li>LaTeX</li>
             <li>Markdown</li>
           </ul>
 
-          <h2 className="font-display">Languages</h2>
+          <h2 className="font-display inline-flex">
+            <ChatIcon className="h-6 w-6 lg:h-10 lg:w-10 mr-4 text-gray-300" />
+            <span>Languages</span>
+          </h2>
           <ul>
             <li>
               <b>English</b> Bilingual, I live aboard, all work done in English
